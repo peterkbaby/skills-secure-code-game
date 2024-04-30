f00e020297073c72ed1e7f3c5a6a003c090b4aae
peterkbaby <84321695+peterkbaby@users.noreply.github.com>
2024-04-30 03:00:44 +0000

    level five is insane

--- Season-1/Level-5/code.py
+++ Season-1/Level-5/code.py
@@ -6,23 +6,25 @@ import binascii
 import random
 import secrets
 import hashlib
+import binascii
+import secrets
+import hashlib
 import os
 import bcrypt
 
 class Random_generator:
 
-    # generates a random token
+    # generates a random token using the secrets library for true randomness
     def generate_token(self, length=8, alphabet=(
     '0123456789'
     'abcdefghijklmnopqrstuvwxyz'
     'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     )):
-        return ''.join(random.choice(alphabet) for _ in range(length))
+        return ''.join(secrets.choice(alphabet) for i in range(length))
 
-    # generates salt
+    # generates salt using the bcrypt library which is a safe implementation
     def generate_salt(self, rounds=12):
-        salt = ''.join(str(random.randint(0, 9)) for _ in range(21)) + '.'
-        return f'$2b${rounds}${salt}'.encode()
+        return bcrypt.gensalt(rounds)
 
 class SHA256_hasher:
 
@@ -38,22 +40,10 @@ class SHA256_hasher:
         password_hash = password_hash.encode('ascii')
         return bcrypt.checkpw(password, password_hash)
 
-class MD5_hasher:
-
-    # same as above but using a different algorithm to hash which is MD5
-    def password_hash(self, password):
-        return hashlib.md5(password.encode()).hexdigest()
-
-    def password_verification(self, password, password_hash):
-        password = self.password_hash(password)
-        return secrets.compare_digest(password.encode(), password_hash.encode())
-
 # a collection of sensitive secrets necessary for the software to operate
 PRIVATE_KEY = os.environ.get('PRIVATE_KEY')
 PUBLIC_KEY = os.environ.get('PUBLIC_KEY')
-SECRET_KEY = 'TjWnZr4u7x!A%D*G-KaPdSgVkXp2s5v8'
-PASSWORD_HASHER = 'MD5_hasher'
-
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
+PASSWORD_HASHER = 'SHA256_hasher'
 # Contribute new levels to the game in 3 simple steps!
 # Read our Contribution Guideline at github.com/skills/secure-code-game/blob/main/CONTRIBUTING.md
